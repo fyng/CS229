3a31879f5fbf0566a7a88552a90717ea42fdfa45
tkim104 <tkim104@jhu.edu>
2022-03-07 21:24:45 -0500

    Adding preliminary files for CSF assignment 3

---
+++ csf_assign03/main.c
@@ -0,0 +1,15 @@
+// Feiyang Huang;
+// Tae Wan Kim; tkim104
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *
+ */
+// Here argc gets the number of arguments received, argv holds the string array of each argument
+int main(int argc, char* argv[]) {
+  if (argc == 6) {
+    
+  }
+  return 0;
+}
