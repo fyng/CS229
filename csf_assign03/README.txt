8162befe1db5956f0ca42ca7d5e230580aeee2a6
tkim104 <tkim104@jhu.edu>
2022-03-07 21:34:16 -0500

    Working makefile and compilable main.c with README.txt

---
+++ csf_assign03/README.txt
@@ -0,0 +1,8 @@
+// Feiyang Huang;
+// Tae Wan Kim; tkim104
+
+AS3MS1:
+
+AS3MS2:
+
+AS3MS3:
--- csf_assign03/main.c
+++ csf_assign03/main.c
@@ -9,7 +9,11 @@
 // Here argc gets the number of arguments received, argv holds the string array of each argument
 int main(int argc, char* argv[]) {
   if (argc == 6) {
-    
+    int checkPowerTwo = atoi(argv[0]);
+    if (checkPowerTwo % 2) {
+      return 0;
+    }
+    return 1;
   }
   return 0;
 }
