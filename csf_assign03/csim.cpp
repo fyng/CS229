d9fa1314caef56818f63ce1b3725f2090cc82648
tkim104 <tkim104@jhu.edu>
2022-03-19 14:11:54 -0400

    Update

--- csf_assign03/csim.cpp
+++ csf_assign03/csim.cpp
@@ -1,75 +1,76 @@
 // Feiyang Huang; fhuang15
 // Tae Wan Kim; tkim104
 #include "cfuncs.h"
-
 #include <iostream>
 #include <string.h>
 using namespace std;
 
 int main (int argc, char* argv[]) {
   // parameter error checking
-    if (argc != 7) {
+  if (argc != 7) {
     std::cerr << "Invalid number of input parameters.\n";
     return 1;
-    }
-
-    int num_set = stoi(argv[1]);
-    int blocks_per_set = stoi(argv[2]);
-    int bytes_per_block = stoi(argv[3]);
-
-    if (num_set < 0 || !isPowTwo(num_set)) {
-      std::cerr << "Invalid number of sets in the cache.\n";
-      return 1;
-    }
-    if (blocks_per_set < 0 || !isPowTwo(blocks_per_set)) {
-      std::cerr << "Invalid number blocks per set.\n";
-      return 1;
-    }
-    if (bytes_per_block < 0 || bytes_per_block < 4 || !isPowTwo(bytes_per_block)) {
-      std::cerr << "Invalid number of bytes per block.\n";
-      return 1;
-    }
-
-    int write_alloc; // 1: write-allocate; 0: no-write-allocate
-    int write_mode;  // 1: write-through; 0: write-back
-    int evic;       // 1: lru; 0: fifo
-
-    string write_miss = argv[4];
-    if (write_miss.compare("write-allocate") == 0){
-      write_alloc = 1;
-    } else if (write_miss.compare("no-write-allocate") == 0) {
-      write_alloc = 0;
+  }
+  
+  int num_set = stoi(argv[1]);
+  int blocks_per_set = stoi(argv[2]);
+  int bytes_per_block = stoi(argv[3]);
+  
+  if (num_set < 0 || !isPowTwo(num_set)) {
+    std::cerr << "Invalid number of sets in the cache.\n";
+    return 1;
+  }
+  if (blocks_per_set < 0 || !isPowTwo(blocks_per_set)) {
+    std::cerr << "Invalid number blocks per set.\n";
+    return 1;
+  }
+  if (bytes_per_block < 4 || !isPowTwo(bytes_per_block)) {
+    std::cerr << "Invalid number of bytes per block.\n";
+    return 1;
+  }
+  
+  int write_alloc; // 1: write-allocate; 0: no-write-allocate
+  int write_mode;  // 1: write-through; 0: write-back
+  int evic;       // 1: lru; 0: fifo
+  
+  string write_miss = argv[4];
+  if (write_miss.compare("write-allocate") == 0){
+    write_alloc = 1;
+  } else if (write_miss.compare("no-write-allocate") == 0) {
+    write_alloc = 0;
+  } else {
+    std::cerr << "Invalid write-miss mode.\n";
+    return 1;
+  }
+  
+  string write_hit = argv[5];
+  if (write_hit.compare("write-through") == 0){
+    write_mode = 1;
+  } else if (write_hit.compare("write-back") == 0) {
+    write_mode = 0;
+  } else {
+    std::cerr << "Invalid write-hit mode.\n";
+    return 1;
+  }
+  
+  string eviction = argv[6];
+  if (eviction.compare("lru") == 0){
+    evic = 1;
+  } else if (eviction.compare("fifo") == 0) {
+    evic = 0;
     } else {
-      std::cerr << "Invalid write-miss mode.\n";
-      return 1;
-    }
+    std::cerr << "Invalid eviction mode.\n";
+    return 1;
+  }
 
-    string write_hit = argv[5];
-    if (write_hit.compare("write-through") == 0){
-      write_mode = 1;
-    } else if (write_hit.compare("write-back") == 0) {
-      write_mode = 0;
-    } else {
-      std::cerr << "Invalid write-hit mode.\n";
-      return 1;
-    }
-
-    string eviction = argv[6];
-    if (eviction.compare("lru") == 0){
-      evic = 1;
-    } else if (eviction.compare("fifo") == 0) {
-      evic = 0;
-    } else {
-      std::cerr << "Invalid eviction mode.\n";
-      return 1;
-    }
-
-    cout << num_set << endl;
-    cout << blocks_per_set << endl;
-    cout << bytes_per_block << endl;
-    cout << write_alloc << endl;
-    cout << write_mode << endl;
-    cout << evic << endl;
-
-    return 0;
+  
+  
+  cout << num_set << endl;
+  cout << blocks_per_set << endl;
+  cout << bytes_per_block << endl;
+  cout << write_alloc << endl;
+  cout << write_mode << endl;
+  cout << evic << endl;
+  
+  return 0;
 }
