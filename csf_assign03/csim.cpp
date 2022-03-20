78ba395ede477a0fdd6edb80c35ff9a920c2c854
tkim104 <tkim104@jhu.edu>
2022-03-19 22:52:04 -0400

    Index fix

--- csf_assign03/csim.cpp
+++ csf_assign03/csim.cpp
@@ -95,7 +95,7 @@ int main (int argc, char* argv[]) {
     ss >> addr;
     uint32_t address = stol(addr, 0 , 16);
     // int offset = (address << (32 - logTwo(bytes_per_block))) >> (32 - logTwo(bytes_per_block));
-    uint32_t index = (address << ((32 - logTwo(bytes_per_block) - logTwo(num_set))) >> (32 - logTwo(bytes_per_block) - logTwo(num_set))) >> (32 - logTwo(num_set));
+    uint32_t index = (address << ((32 - logTwo(bytes_per_block) - logTwo(num_set))) >> (32 - logTwo(num_set)));
     uint32_t tag = address >> (logTwo(bytes_per_block) + logTwo(num_set));
 
     // Create the Block struct
