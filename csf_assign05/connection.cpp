a1cec9ed9727cafc729fbc75c98cd15d0fdfc1f5
Feiyang <feiyangthegoat@gmail.com>
2022-04-19 19:03:03 -0400

    prototype send/receive

--- csf_assign05/connection.cpp
+++ csf_assign05/connection.cpp
@@ -27,11 +27,13 @@ void Connection::connect(const std::string &hostname, int port) {
 
   m_fd = open_clientfd(hostnm, p.str().c_str());
   if (m_fd == -2){
+    // handle error
   } else if (m_fd == -1){
+    // handle error
   } 
 
   // assume successful open
-  rio_readinitb(&m_fdbuf, m_fd);
+  Rio_readinitb(&m_fdbuf, m_fd);
 }
 
 Connection::~Connection() {
@@ -55,10 +57,43 @@ bool Connection::send(const Message &msg) {
   // TODO: send a message
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
+  int msg_len = msg->tag.length() + msg->data.length() + 1;
+  std::string msg;
+  msg << msg->tag << ":" << msg->data;
+  ssize_t write_len = rio_writen(m_fd, &msg[0], msg_len);
+
+  if (write_len == -1){
+    m_last_result(EOF_OR_ERROR);
+    return false;
+  }
+  m_last_result(SUCCESS);
+  return true;
 }
 
 bool Connection::receive(Message &msg) {
-  // TODO: send a message, storing its tag and data in msg
+  // TODO: receive a message, storing its tag and data in msg
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
+  int len = msg->MAX_LEN;
+  std::string read_buf; 
+  ssize_t read_len = Rio_readlineb(m_fdbuf, read_buf, msg->MAX_LEN);
+  if (read_len == 0){
+     m_last_result(EOF_OR_ERROR);
+    return false;   
+  }
+
+  read_buf = trim(read_buf);
+  std::string delim = ":";
+  size_t found = read_buf.find(delim);
+  if (found == std::string::npos){
+    // invalid message: no ":"
+    m_last_result(INVALID_MSG);
+    return false;
+  }
+  msg->tax = read_buf.substr(0, found);
+  read_buf.erase(0, found + delim.length());
+  msg->data = read_buf;
+
+  m_last_result(SUCCESS);
+  return true;
 }
