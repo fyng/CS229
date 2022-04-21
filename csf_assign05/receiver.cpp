1d3d1a7b4e6fffce53d92f4fa7964e01a33f4765
Feiyang <feiyangthegoat@gmail.com>
2022-04-21 14:40:32 -0400

    fix receiver connect and join

--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -18,73 +18,59 @@ int main(int argc, char **argv) {
   std::string username = argv[3];
   std::string room_name = argv[4];
 
-  Connection conn;
-
   // TODO: connect to server
-
-  // Set up connection to server using connect function
+  Connection conn;
   conn.connect(server_hostname, server_port); 
   
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
-
-  // Send username info
-  Message username_message = Message(TAG_RLOGIN, username);
-  conn.send(username_message);
-
-  // See if rlogin was successful
-  Message username_check;
-  if (!conn.receive(username_check)) {
-    std::cout << "Failed Receiver Login" << std::endl;
+  Send(&conn, TAG_RLOGIN, username);
+  if (Receive(&conn)){
     return 1;
   }
-  // Receive returned properly, check answer
-  std::vector<std::string> received_message;
-  received_message = username_check.split_payload();
-  // Error, something went wrong server side or OK, else unknown behavior
-  if (received_message[0].compare(TAG_ERR) == 0) {
-    std::cerr << received_message[1] << std::endl;
-    std::cout << received_message[1] << std::endl;
+  Send(&conn, TAG_JOIN, room_name);
+  if (Receive(&conn)){
     return 1;
-  } else if (received_message[0].compare(TAG_OK) == 0) {
-    std::cout << received_message[1] << std::endl;
-  } else {
-    // Should have received OK, if not, unknown behavior
-    std::cerr << "Unknown/Invalid Tag received" << std::endl;
-    return 2;
   }
 
-  // Send room join info
-  Message room_message = Message(TAG_JOIN, room_name);
-  conn.send(room_message);
+  // // Receive returned properly, check answer
+  // std::vector<std::string> received_message;
+  // received_message = username_check.split_payload();
+  // // Error, something went wrong server side or OK, else unknown behavior
+  // if (received_message[0].compare(TAG_ERR) == 0) {
+  //   std::cerr << received_message[1] << std::endl;
+  //   std::cout << received_message[1] << std::endl;
+  //   return 1;
+  // } else if (received_message[0].compare(TAG_OK) == 0) {
+  //   std::cout << received_message[1] << std::endl;
+  // } else {
+  //   // Should have received OK, if not, unknown behavior
+  //   std::cerr << "Unknown/Invalid Tag received" << std::endl;
+  //   return 2;
+  // }
 
-  // See if room join was successful
-  Message rec_room_mess;
-  if (!conn.receive(rec_room_mess)) {
-    std::cerr << "Failed to Join Room" << std::endl;
-    return 1;
-  }
-  // Receive returned properly, received reply message
-  received_message = rec_room_mess.split_payload();
-  // Error, something went wrong server side or OK, else unknown behavior
-  if (received_message[0].compare(TAG_ERR) == 0) {
-    std::cerr << received_message[1] << std::endl;
-    std::cout << received_message[1] << std::endl;
-    return 1;
-  } else if (received_message[0].compare(TAG_OK) == 0) {
-    std::cout << received_message[1] << std::endl;
-  } else {
-    // Should have received OK, if not, unknown behavior
-    std::cerr << "Uknown/Invalid Tag received" << std::endl;
-    return 2;
-  }
+  // // Receive returned properly, received reply message
+  // received_message = rec_room_mess.split_payload();
+  // // Error, something went wrong server side or OK, else unknown behavior
+  // if (received_message[0].compare(TAG_ERR) == 0) {
+  //   std::cerr << received_message[1] << std::endl;
+  //   std::cout << received_message[1] << std::endl;
+  //   return 1;
+  // } else if (received_message[0].compare(TAG_OK) == 0) {
+  //   std::cout << received_message[1] << std::endl;
+  // } else {
+  //   // Should have received OK, if not, unknown behavior
+  //   std::cerr << "Uknown/Invalid Tag received" << std::endl;
+  //   return 2;
+  // }
   
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
 
   // Set while loop to receive and print sent info
+  Message server_message;
+  std::vector<std::string> received_message;
   while (1) {
-    Message server_message;
     if (!conn.receive(server_message)) {
       std::cerr << "Failed to receive any message" << std::endl;
       return 1;
@@ -92,12 +78,9 @@ int main(int argc, char **argv) {
     // If receive did not fail, then print sent message
     received_message = server_message.split_payload();
     if (received_message[0].compare(TAG_DELIVERY) == 0) {
-      std::cout << received_message[2]
-		<< ":"
-		<< received_message[3]
-		<< std::endl;
+      std::cout << received_message[2]	<< ":"	<< received_message[3];
     } else if (received_message[0].compare(TAG_ERR) == 0) {
-      std::cout << received_message[1] << std::endl;
+      std::cout << received_message[1];
     }
   }
 
