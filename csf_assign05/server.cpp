63f1eb53d949acdf4601e90770f09065e95b1a11
Feiyang <feiyangthegoat@gmail.com>
2022-04-27 21:39:11 -0400

    add sample code from class website

--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -19,6 +19,17 @@
 
 // TODO: add any additional data types that might be helpful
 //       for implementing the Server member functions
+struct ConnInfo {
+  int clientfd;
+  Connection conn; 
+
+  ConnInfo(Connection *conn, Server *server) : conn(conn), server(server) { }
+  ~ConnInfo() {
+    // destroy connection when ConnInfo object is destroyed
+    delete conn;
+  }
+};
+
 
 ////////////////////////////////////////////////////////////////////////
 // Client thread functions
@@ -32,10 +43,47 @@ void *worker(void *arg) {
   // TODO: use a static cast to convert arg from a void* to
   //       whatever pointer type describes the object(s) needed
   //       to communicate with a client (sender or receiver)
+  struct ConnInfo *info_ = static_cast<ConnInfo *>(arg);
+  // use a std::unique_ptr to automatically destroy the ConnInfo object
+  // when the worker function finishes; this will automatically ensure
+  // that the Connection object is destroyed
+  std::unique_ptr<ConnInfo> info(info_);
 
   // TODO: read login message (should be tagged either with
   //       TAG_SLOGIN or TAG_RLOGIN), send response
+  Message msg;
 
+  if (!info->conn->receive(msg)) {
+    if (info->conn->get_last_result() == Connection::INVALID_MSG) {
+      info->conn->send(Message(TAG_ERR, "invalid message"));
+    }
+    return nullptr;
+  }
+
+  if (msg.tag != TAG_SLOGIN && msg.tag != TAG_RLOGIN) {
+    info->conn->send(Message(TAG_ERR, "first message should be slogin or rlogin"));
+    return nullptr;
+  }
+
+  std::string username = msg.data;
+  if (!info->conn->send(Message(TAG_OK, "welcome " + username))) {
+    return nullptr;
+  }
+
+  // Just loop reading messages and sending an ok response for each one
+  while (true) {
+    if (!info->conn->receive(msg)) {
+      if (info->conn->get_last_result() == Connection::INVALID_MSG) {
+        info->conn->send(Message(TAG_ERR, "invalid message"));
+      }
+      break;
+    }
+
+    if (!info->conn->send(Message(TAG_OK, "this is just a dummy response"))) {
+      break;
+    }
+  }
+  
   // TODO: depending on whether the client logged in as a sender or
   //       receiver, communicate with the client (implementing
   //       separate helper functions for each of these possibilities
@@ -54,22 +102,20 @@ Server::Server(int port)
   : m_port(port)
   , m_ssock(-1) {
   // TODO: initialize mutex
+  pthread_mutex_init(&m_lock, nullptr);
 }
 
 Server::~Server() {
   // TODO: destroy mutex
+  pthread_mutex_destroy(&m_lock);
 }
 
 bool Server::listen() {
   // TODO: use open_listenfd to create the server socket, return true
   //       if successful, false if not
-  int socket = open_listenfd(m_port);
-  if (socket <  0) {
-    return false;
-  } else {
-    m_ssock = socket;
-  }
-  return true;
+  std::string port = std::to_string(m_port);
+  m_ssock = open_listenfd(m_port.c_str());
+  return m_ssock >= 0;
 }
 
 void Server::handle_client_requests() {
@@ -84,7 +130,18 @@ void Server::handle_client_requests() {
   
   while (1) {
     //wait_for_avail_proc();
-    int client_fd = Accept(m_ssock, NULL, NULL);
+    int clientfd = accept(m_ssock, NULL, NULL);
+    if (clientfd < 0) {
+      std::cerr << "Error accepting connection\n";
+      return;
+    }
+
+    ConnInfo *info = new ConnInfo(new Connection(clientfd), this);
+    pthread_t thread_id;
+    if (pthread_create(&thread_id, NULL, worker, static_cast<void *>(info)) != 0) {
+      std::cerr << "could not create thread\n";
+      return;
+    }
 
   }
 }
@@ -92,6 +149,24 @@ void Server::handle_client_requests() {
 Room *Server::find_or_create_room(const std::string &room_name) {
   // TODO: return a pointer to the unique Room object representing
   //       the named chat room, creating a new one if necessary
+
+  // this function can be called from multiple threads, so
+  // make sure the mutex is held while accessing the shared
+  // data (the map of room names to room objects)
+  Guard g(m_lock);
+
+  Room *room;
+
+  auto i = m_rooms.find(room_name);
+  if (i == m_rooms.end()) {
+    // room does not exist yet, so create it and add it to the map
+    room = new Room(room_name);
+    m_rooms[room_name] = room;
+  } else {
+    room = i->second;
+  }
+
+  return room;
 }
 
 
